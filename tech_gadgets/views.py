8172f5b85250db3ce9bfe1adf3fb709be64862c9
Springer Johannes (EcP/TEF7) <Johannes.Springer@boschrexroth.de>
2025-04-10 12:38:14 +0200

    testing slugs

--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import json
+from django.utils.text import slugify
 
 from .dummy_data import gadgets
 # Create your views here.
@@ -8,5 +9,13 @@ from .dummy_data import gadgets
 def start_page_view(request):
     return HttpResponse("Griaß de")
 
-def single_gadget_view(request):
-    return JsonResponse(gadgets[0])
+def single_gadget_view(request, gadget_id):
+    return JsonResponse(gadgets[gadget_id])
+
+def single_gadget_slug_view(request, gadget_slug):
+    gadget_match = {"result": "nothing"}
+    
+    for gadget in gadgets:
+        if slugify(gadget["name"]) == gadget_slug:
+            gadget_match = gadget
+    return JsonResponse(gadget_match)
