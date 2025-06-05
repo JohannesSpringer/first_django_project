8d01a0ea7a519dc1d4530e236a286c0509fa4edb
Springer Johannes (EcP/TEF7) <Johannes.Springer@boschrexroth.de>
2025-06-05 12:57:28 +0200

    added base view and redirect view

--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -4,35 +4,63 @@ import json
 from django.utils.text import slugify
 from django.urls import reverse
 
+from django.views import View
+from django.views.generic.base import RedirectView
+
 from .dummy_data import gadgets
+
 # Create your views here.
+
 
 def start_page_view(request):
     return HttpResponse("Griaß de")
 
-def single_gadget_view(request, gadget_id):
+
+def single_gadget_int_view(request, gadget_id):
     if len(gadgets) > gadget_id:
         new_slug = slugify(gadgets[gadget_id]["name"])
         new_url = reverse("gadget_slug_url", args=[new_slug])
         return redirect(new_url)
     return HttpResponseNotFound("Not found")
 
-def single_gadget_slug_view(request, gadget_slug):
-    gadget_match = None
-    
-    for gadget in gadgets:
-        if slugify(gadget["name"]) == gadget_slug:
-            gadget_match = gadget
-            
-    if gadget_match:
-        return JsonResponse(gadget_match)
-    raise Http404()
 
-def single_gadget_post_view(request):
-    if request.method == "POST":
+class RedirectToGadgetView(RedirectView):
+    pattern_name = "gadget_slug_url"
+
+    def get_redirect_url(self, *args, **kwargs):
+        slug = slugify(gadgets[kwargs.get("gadget_id", 0)]["name"])
+        new_kwargs = {"gadget_slug": slug}
+        return super().get_redirect_url(*args, **new_kwargs)
+
+
+class GadgetView(View):
+
+    def get(self, request, gadget_slug=""):
+        gadget_match = None
+        for gadget in gadgets:
+            if slugify(gadget["name"]) == gadget_slug:
+                gadget_match = gadget
+
+        if gadget_match:
+            return JsonResponse(gadget_match)
+        raise Http404()
+
+    def post(self, request, *args, **kwargs):
         try:
             data = json.loads(request.body)
             print(f"Received data: {data}")
             return JsonResponse({"response": "Post erfolgreich"})
         except:
             return JsonResponse({"response": "Post nicht möglich"})
+
+
+def single_gadget_slug_view(request, gadget_slug):
+    gadget_match = None
+
+    for gadget in gadgets:
+        if slugify(gadget["name"]) == gadget_slug:
+            gadget_match = gadget
+
+    if gadget_match:
+        return JsonResponse(gadget_match)
+    raise Http404()
